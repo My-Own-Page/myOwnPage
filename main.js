b71944acdb5701f623da794b50e2759d289631c6
yongseopK <kk0@kakao.com>
2023-08-17 13:07:21 +0900

    메인 업데이트

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ $searchBtn.addEventListener("click", () => {
 });
 
 searchInput.addEventListener("keydown", (e) => {
-    if (e.key === 'Enter') {
-        performSearch();
+    if (!e.isComposing) {
+        if (e.key === 'Enter') {
+            performSearch();
+        }
     }
 });
 
@@ -17,7 +19,32 @@ function performSearch() {
     const searchValue = searchInput.value;
     const googleSearch = `https://www.google.com/search?q=${searchValue}`;
 
-    window.open(googleSearch);
+    if (!searchValue == '') {
+        window.open(googleSearch);
+    } else {
+        alert('비어있으면 안됩니다!');
+    }
 
     searchInput.value = '';
 }
+
+
+// 다른 파일 갖다 붙히는 부분
+const musicPlayer = document.querySelector('.musicPlayer');
+
+fetch('./MusicPlayer/index.html')
+    .then(res => res.text())
+    .then(data => {
+        // Extract the body content from the loaded HTML
+        const bodyContent = data.match(/<body[^>]*>([\s\S]*)<\/body>/i);
+
+        if (bodyContent) {
+            musicPlayer.innerHTML = bodyContent[1];
+
+            // Load the associated CSS separately
+            const cssLink = document.createElement('link');
+            cssLink.rel = 'stylesheet';
+            cssLink.href = './MusicPlayer/assets/styles/app.css'; // Adjust the path based on your project structure
+            document.head.appendChild(cssLink);
+        }
+    });
