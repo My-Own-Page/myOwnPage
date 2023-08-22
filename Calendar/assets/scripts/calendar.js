3169c92b5ebe6ea59e6a44316577fe8915e27f85
Fasa <rudrbgus3@gmail.com>
2023-08-22 16:38:49 +0900

    calendar - 모달 부분
    - 날짜 클릭하면 모달 띄우고
    ※ x버튼과 overlay 부분 preventDefault해야함 잘안돼

--- Calendar/assets/scripts/calendar.js
+++ Calendar/assets/scripts/calendar.js
@@ -15,18 +15,17 @@ const prevMonthHandler = (e) => {
       month: --month_, //7 + 1
       date: date_,
     })
-    );
-    for(i=1;i<=42;i++){
-      document.querySelector(`.date${i}`).classList.remove('selectdate');
-    }
-    
+  );
+  for (i = 1; i <= 42; i++) {
+    document.querySelector(`.date${i}`).classList.remove('selectdate');
+  }
 };
 
 const nextMonthHandler = (e) => {
-    if (month_ === 12) {
-        year_ += 1;
-        month_ = 0;
-      }
+  if (month_ === 12) {
+    year_ += 1;
+    month_ = 0;
+  }
   renderCalendar(
     calcDate({
       year: year_,
@@ -34,16 +33,30 @@ const nextMonthHandler = (e) => {
       date: date_,
     })
   );
-  for(i=1;i<=42;i++){
+  for (i = 1; i <= 42; i++) {
     document.querySelector(`.date${i}`).classList.remove('selectdate');
   }
 };
 
-const clickDateHandler = (e) =>{
-  e.target.closest('.date-box').querySelector(`.date${selectdateId}`).classList.remove('selectdate');
-  e.target.classList.add('selectdate');  
+const clickDateHandler = (e) => {
+  e.target
+    .closest('.date-box')
+    .querySelector(`.date${selectdateId}`)
+    .classList.remove('selectdate');
+  e.target.classList.add('selectdate');
   selectdateId = e.target.dataset.id;
-}
+  const $calendarModal = document.getElementById('calendar-modal');
+  const $calendarModalOverlay = document.querySelector('.modal-overlay');
+  $calendarModalOverlay.style.display = 'flex';
+  $calendarModal.style.display = 'flex';
+
+  renderModal({
+    year: year_,
+    month: month_,
+    date: Number(e.target.textContent),
+    selectDateId: selectdateId,
+  });
+};
 
 // ---------------------------------Function------------------------------- //
 const calcDate = ({ year, month, date }) => {
@@ -67,7 +80,7 @@ const calcDate = ({ year, month, date }) => {
 
   let calendarMonthLastDay = monthDays[calendarMonth - 1]; // 마지막날
   let calendarStartMonthDay =
-    new Date(calendarYear, calendarMonth - 1, 1).getDay() + 1; //처음날이 무슨요일인지 1~7 일~토  
+    new Date(calendarYear, calendarMonth - 1, 1).getDay() + 1; //처음날이 무슨요일인지 1~7 일~토
   return {
     startDay: calendarStartMonthDay,
     lastDay: calendarMonthLastDay,
@@ -83,19 +96,36 @@ const makeDate = () => {
   const $dateBox = document.querySelector('.date-box');
   // console.log($dateBox);
   for (let i = 1; i <= 42; i++) {
-    const $emptyDate = document.createElement('div');    
-    if(i===1 || i===8 || i === 15|| i === 22 || i===29 || i===36){              
-        $emptyDate.classList.add('red');
+    const $emptyDate = document.createElement('div');
+    if (i === 1 || i === 8 || i === 15 || i === 22 || i === 29 || i === 36) {
+      $emptyDate.classList.add('red');
     }
-    if(i===7 || i===14 || i === 21|| i === 28 || i===35 || i===42){            
-        $emptyDate.classList.add('blue');
-      }
-    
-    $emptyDate.classList.add('date');    
+    if (i === 7 || i === 14 || i === 21 || i === 28 || i === 35 || i === 42) {
+      $emptyDate.classList.add('blue');
+    }
+
+    $emptyDate.classList.add('date');
     $emptyDate.classList.add(`date${i}`);
-    $emptyDate.dataset.id = i;    
+    $emptyDate.dataset.id = i;
     $emptyDate.addEventListener('click', clickDateHandler);
     $dateBox.appendChild($emptyDate);
+  }
+
+  const $dateBox2 = document.querySelector('.body__date-box');
+  for (let i = 1; i <= 42; i++) {
+    const $emptyDate = document.createElement('div');
+    if (i === 1 || i === 8 || i === 15 || i === 22 || i === 29 || i === 36) {
+      $emptyDate.classList.add('red');      
+    }
+    if (i === 7 || i === 14 || i === 21 || i === 28 || i === 35 || i === 42) {
+      $emptyDate.classList.add('blue');
+    }
+    $emptyDate.classList.add('date');
+    $emptyDate.classList.add(`date${i}`);
+    $emptyDate.textContent ='날짜';
+    $emptyDate.dataset.id = i;
+    // $emptyDate.addEventListener('click', clickDateHandler);
+    $dateBox2.appendChild($emptyDate);
   }
 };
 
@@ -148,30 +178,35 @@ const renderYearTitle = () => {
   $yearBox.textContent = `${year_}년`;
 };
 
-const insertDate = ({ startDay, lastDay, monthDays, month }) => {
-  const $dateBox = document.querySelector('.date-box');
+const insertDate = ({ startDay, lastDay, monthDays, month, dateBoxName }) => {
+  const $dateBox = document.querySelector(dateBoxName);
   //이번달 날짜 만들기
   let startDate = 1;
-  
+
   for (let i = startDay; i <= lastDay + startDay - 1; i++) {
+    const $dateElement = $dateBox.querySelector(`.date${i}`);
+    if($dateElement){
+    $dateElement.textContent = `${startDate}`;  
     $dateBox.querySelector(`.date${i}`).textContent = `${startDate}`;
     $dateBox.querySelector(`.date${i}`).classList.remove('prevMonth');
     $dateBox.querySelector(`.date${i}`).classList.remove('nextMonth');
-    if(i-startDay+1===date_){
+    
+    if (i - startDay + 1 === date_) {
       $dateBox.querySelector(`.date${i}`).classList.add('selectdate');
     }
     startDate++;
-    
+    }
   }
   //지난달 날짜 만들기
-  if(month===1){
-    month=13;
-  };    
-  let lastDate = monthDays[month - 2];  
-  for (let i = startDay - 1; i > 0; i--) {
+  if (month === 1) {
+    month = 13;
+  }
+  let lastDate = monthDays[month - 2];
+  for (let i = startDay - 1; i > 0; i--) {    
     $dateBox.querySelector(`.date${i}`).textContent = `${lastDate}`;
     $dateBox.querySelector(`.date${i}`).classList.add('prevMonth');
     lastDate--;
+  
   }
 
   //다음달 날짜 만들기
@@ -184,26 +219,86 @@ const insertDate = ({ startDay, lastDay, monthDays, month }) => {
 };
 
 const renderCalendar = ({ startDay, lastDay, monthDays, month }) => {
-  insertDate({ startDay, lastDay, monthDays, month });
+  insertDate({ startDay, lastDay, monthDays, month, dateBoxName: '.date-box' });
   renderMonthTitle(month);
   renderYearTitle();
-  document.querySelector('.prev-button').addEventListener('click', prevMonthHandler);    
-  document.querySelector('.next-button').addEventListener('click', nextMonthHandler);         
-  selectdateId = date_ + startDay - 1;    
+  document
+    .querySelector('.prev-button')
+    .addEventListener('click', prevMonthHandler);
+  document
+    .querySelector('.next-button')
+    .addEventListener('click', nextMonthHandler);
+  selectdateId = date_ + startDay - 1;
 };
 
-const selectToday = () =>{
+const renderModal = ({ year, month, date, selectDateId }) => {
+  
 
-}
+  const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 === 0) {
+    monthDays[1] = 29;
+  } else if (year % 100 === 0) {
+    monthDays[1] = 28;
+  } else if (year % 4 === 0) {
+    monthDays[1] = 29;
+  }
+
+  console.log(`${year}년${month}월${date}일 ${selectDateId}`);
+
+  const renderModalYearAndMonth = () => {
+    const $modalYearTitle = document.querySelector(
+      '.header__year-and-month .header__year'
+    );
+    const $modalMonthTitle = document.querySelector(
+      '.header__year-and-month .header__month'
+    );
+    $modalYearTitle.textContent = `${year}년`;
+    $modalMonthTitle.textContent = `${month}월`;
+  };
+
+  const renderModalDate = () => {
+    insertDate({
+      startDay: selectDateId - date + 1,
+      lastDay: monthDays[month - 1],
+      monthDays: monthDays,
+      month,      
+      dateBoxName: '.body__date-box'
+    });
+  };
+
+  const exitModal = () =>{
+    const $modalOverlay = document.querySelector('.modal-overlay');
+    const $calendarModal = document.getElementById('calendar-modal');
+
+    const $exitButton = document.querySelector('.header__exit-button').
+    addEventListener('click', e=>{
+      console.log(e);      
+      e.stopPropagation();
+      $modalOverlay.style.display = 'none';
+      $calendarModal.style.display = 'none';         
+    });
+    $modalOverlay.addEventListener('click', e=>{
+      e.stopPropagation();
+      $modalOverlay.style.display = 'none';
+      $calendarModal.style.display = 'none';         
+    })
+  };
+
+  renderModalYearAndMonth();
+  renderModalDate();
+  exitModal();
+};
 
 // ------------------------- 실행부 ----------------------------
 const start = () => {
   makeDate();
-  renderCalendar(calcDate({
-    year: year_,
-    month: month_, //7 + 1
-    date: date_,
-  }));
+  renderCalendar(
+    calcDate({
+      year: year_,
+      month: month_, //7 + 1
+      date: date_,
+    })
+  );
 };
-  
+
 start();
