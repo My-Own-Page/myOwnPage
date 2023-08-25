17763d18c67640343ebb74cee8fa00bea44b9a9e
Fasa <rudrbgus3@gmail.com>
2023-08-25 16:08:05 +0900

    calendar - 최적화 진행중 60%

--- Calendar/assets/scripts/newCalendar.js
+++ Calendar/assets/scripts/newCalendar.js
@@ -27,10 +27,39 @@ const calendarFunc = () => {
   let presentMonth = 0;
   let presentDate = 0;
   let dateId = '';
+  let selecetDateId='';
   let monthStartDay = 0; // 달에 시작 시작요일
-  let monthLastDate = 0; // 달에 마지막 날    
+  let monthLastDate = 0; // 달에 마지막 날  
+
+   
+  const selectDateTint = () =>{    
+    const $calendar = document.getElementById('calendar-box'); 
+    const $calendarModal = document.getElementById('calendar-modal');            
+    for(i=0;i<42;i++){            
+      if($calendar.querySelector(`.date${i}`).dataset.id === selecetDateId){
+        $calendar.querySelector(`.date${i}`).classList.add('selected');
+        $calendarModal.querySelector(`.date${i}`).classList.add('selected');
+      }else{
+        $calendar.querySelector(`.date${i}`).classList.remove('selected');
+        $calendarModal.querySelector(`.date${i}`).classList.remove('selected');
+      }
+    }
+  };
 
 
+  const clickDateHandler = e =>{
+    presentDate =e.target.textContent;
+    const $calendarModal = document.getElementById('calendar-modal');
+    selecetDateId=e.target.dataset.id;
+    console.log(selecetDateId);
+    selectDateTint();
+    $calendarModal.style.display = 'flex';
+    console.log(presentDate);
+    setDateCotents('#calendar-modal .calendar-modal__body .body__date-box');
+    setTitleYear('#calendar-modal .calendar-modal__header .header__year-and-month');
+    setTitleMonth('#calendar-modal .calendar-modal__header .header__year-and-month');    
+    e.preventDefault();
+  };
   const leapYear = year => {
     if (year % 400 === 0) {
       monthLastDates[1] = 29;
@@ -69,13 +98,12 @@ const calendarFunc = () => {
       $emptyDate.classList.add(`date${i}`);
       $emptyDate.textContent=`${i}번째 인덱스`;
       $emptyDate.dataset.id = 0;
-      // $emptyDate.addEventListener('click', clickDateHandler);
-      $monthBox.appendChild($emptyDate);
-      
+      $emptyDate.addEventListener('click', clickDateHandler);
+      $monthBox.appendChild($emptyDate);      
     }
   };
   const setTitleYear = (yearSelector) => {
-    const $monthBox = document.querySelector(yearSelector);
+    const $monthBox = document.querySelector(yearSelector);    
     const $yearBox = $monthBox.querySelector('h1');
     $yearBox.textContent = `${presentYear}년`;
   };
@@ -182,6 +210,7 @@ const calendarFunc = () => {
       setTitleYear('.monthAndYear');
       setTitleMonth('.monthAndYear');
       setDateCotents('#calendar-box .date-box');
+      selectDateTint();
     }
     const nextMonthHandler =() =>{
       if(presentMonth===11) {
@@ -194,17 +223,140 @@ const calendarFunc = () => {
       setTitleYear('.monthAndYear');
       setTitleMonth('.monthAndYear');
       setDateCotents('#calendar-box .date-box');
+      selectDateTint();
     };
+
 
     const $prevButton = document.getElementById('calendar-box').querySelector('.prev-button');
     const $nextButton = document.getElementById('calendar-box').querySelector('.next-button');
     $nextButton.addEventListener('click', nextMonthHandler);
     $prevButton.addEventListener('click', prevMonthHandler);
   };  
+  const calendarModalFunc =() =>{    
+    renderDates('#calendar-modal .calendar-modal__body .body__date-box');
+    setDateCotents('#calendar-modal .calendar-modal__body .body__date-box');
+    setTitleYear('#calendar-modal .calendar-modal__header .header__year-and-month');
+    setTitleMonth('#calendar-modal .calendar-modal__header .header__year-and-month');
+
+
+    const modalInit = () =>{            
+      const $modalPrevButton = document.getElementById('calendar-modal').querySelector('.header__prev-button');
+      const $modalNextButton = document.getElementById('calendar-modal').querySelector('.header__next-button');
+      const $modalExitButton = document.getElementById('calendar-modal').querySelector('.header__exit-button');
+      const $modalAddListButton = document.getElementById('calendar-modal').querySelector('.add-button');
+      const modalPrevMonthHandler = () => {
+        if(presentMonth===0) {
+          presentMonth=12;
+          presentYear-=1;
+          leapYear(presentYear);
+        }
+        presentMonth-=1;
+        setStartDayAndEndDate(presentYear, presentMonth);
+        setTitleYear('#calendar-modal .calendar-modal__header .header__year-and-month');
+        setTitleMonth('#calendar-modal .calendar-modal__header .header__year-and-month');
+        setDateCotents('#calendar-modal .calendar-modal__body .body__date-box');        
+        selectDateTint();          
+      };
+      const modalNextMonthHandler = () =>{
+        if(presentMonth===11) {
+          presentMonth=-1;
+          presentYear+=1;
+          leapYear(presentYear);
+        }
+        presentMonth+=1;
+        setStartDayAndEndDate(presentYear, presentMonth);
+        setTitleYear('#calendar-modal .calendar-modal__header .header__year-and-month');
+        setTitleMonth('#calendar-modal .calendar-modal__header .header__year-and-month');
+        setDateCotents('#calendar-modal .calendar-modal__body .body__date-box');
+        selectDateTint();
+      };
+      const modalExitHandler = e => {
+        const $calendarModal = document.getElementById('calendar-modal');
+        $calendarModal.style.display = 'none';
+        setTitleYear('.monthAndYear');
+        setTitleMonth('.monthAndYear');   
+        setDateCotents('#calendar-box .date-box');
+        e.preventDefault();
+      };
+      const showAddListHandler = () =>{
+        const $addListModal = document.querySelector('#calendar-modal .add-list-modal');
+        $addListModal.style.display = 'flex';
+      }
+      
+      
+      $modalPrevButton.addEventListener('click', modalPrevMonthHandler);
+      $modalNextButton.addEventListener('click', modalNextMonthHandler);
+      $modalExitButton.addEventListener('click', modalExitHandler);
+      $modalAddListButton.addEventListener('click', showAddListHandler);
+    };
+    // ------------------------------ 일정추가 모달 -----------------------------------
+    const addListModalFunc = () =>{
+      const $addListModal = document.querySelector('.add-list-modal')
+      const $addListExitButton = document.querySelector('#calendar-modal .add-list-modal button.exit-button');
+      const $addListAddButton = document.querySelector('#calendar-modal .add-list-modal .list-context-container .list-context-button');    
+      // 일정 추가 모달 나가기
+      const exitAddListHandler = (e) =>{
+        const $addListModal = document.querySelector('#calendar-modal .add-list-modal');
+        $addListModal.style.display = 'none';
+        e.preventDefault();
+      };
+      // 일정 추가 모달에서 일정 넘기기
+      const addDateListHandler = (e) =>{      
+        e.preventDefault();
+        const $startDate = $addListModal.querySelector('.start-date');
+        const $endDate = $addListModal.querySelector('.end-date');
+        const $startTime = $addListModal.querySelector('.start-time');
+        const $endTime = $addListModal.querySelector('.end-time');
+        const $listContext = $addListModal.querySelector('.list-context');
+        console.log(`startDate: ${$startDate.value}, endDate: ${$endDate.value}, startTime: ${$startTime.value}, endTime: ${$endTime.value}`);
+        const whatIsStartYearMonthDate = $startDate.value.split('-');      
+        const whatIsEndYearMonthDate = $endDate.value.split('-');
+        const whatIsStartTime = $startTime.value.split(':');
+        const whatIsEndTime = $endTime.value.split(':');
+        const startYear =  whatIsStartYearMonthDate[0];
+        const startMonth = whatIsStartYearMonthDate[1];
+        const startDate = whatIsStartYearMonthDate[2];
+        const endYear = whatIsEndYearMonthDate[0];
+        const endMonth = whatIsEndYearMonthDate[1];
+        const endDate = whatIsEndYearMonthDate[2];
+        const startHour = whatIsStartTime[0];
+        const startMinute = whatIsStartTime[1];
+        const endHour = whatIsEndTime[0];
+        const endMinute = whatIsEndTime[1];
+        const context = $listContext.value;
+        schedule.push({
+          key: $startDate.value,
+          startYear,
+          startMonth,
+          startDate,
+          endYear,
+          endMonth,
+          endDate,
+          startHour,
+          startMinute,
+          endHour,
+          endMinute,
+          context 
+        });
+        console.log(schedule);
+        $addListModal.style.display = 'none';
+      }  
+      $addListExitButton.addEventListener('click', exitAddListHandler);
+      $addListAddButton.addEventListener('click', addDateListHandler);
+  
+    };
+
+
+    modalInit();
+    addListModalFunc();
+  };
+
   const start = () =>{
     presentYear = todayYear;
     presentMonth = todayMonth;
     presentDate = todayDate;
+    calendarModalFunc();
+
     setDateId();
     renderDates('#calendar-box .date-box');
     leapYear(todayYear);
